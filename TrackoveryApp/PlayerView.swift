8f561c643ff9c42b81b6d7862381b523dc3d97b0
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-08-09 22:14:37 +0200

    - Implementing Player Interface

---
+++ TrackoveryApp/PlayerView.swift
@@ -0,0 +1,91 @@
+//
+//  PlayerView.swift
+//  TrackoveryApp
+//
+//  Created by Bochkarov Valentyn on 09/08/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 24) {
+                Image(systemName: "a.square")
+                    .resizable()
+                    .frame(width: geometry.size.width - 48, height: geometry.size.width - 48)
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                
+                VStack(spacing: 8) {
+                    Text("Song Title")
+                        .font(Font.system(.title).bold())
+                    Text("Artist Name")
+                        .font(.system(.headline))
+                }
+                HStack(spacing: 40) {
+                    Button(action: {
+                        print("Rewind")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName: "backward.fill")
+                                .foregroundColor(.white)
+                                .font(.system(.title))
+                        }
+                    }
+                    Button(action: {
+                        print("Pause")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName: "pause.fill")
+                                .foregroundColor(.white)
+                                .font(.system(.title))
+                        }
+                    }
+                    Button(action: {
+                        print("Skip")
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                                .accentColor(.pink)
+                                .shadow(radius: 10)
+                            Image(systemName: "forward.fill")
+                                .foregroundColor(.white)
+                                .font(.system(.title))
+                        }
+                    }
+                }
+                Button(action: {
+                    print("Skip")
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .accentColor(.green)
+                            .shadow(radius: 10)
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .font(.system(.title))
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
